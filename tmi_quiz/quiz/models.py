43c8d09777cd31ac1520c8d2ffc249ed78fada35
jihoo <ahrefhoo@naver.com>
2024-05-12 21:47:07 +0900

    feat: 추가 선택지 클릭시 페이지 이동 및 정답처리

--- tmi_quiz/quiz/models.py
+++ tmi_quiz/quiz/models.py
@@ -2,8 +2,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
     comment = models.TextField('하고 싶은 말')
+    answers = models.CharField(max_length=25)
+    # "1번점수-2번점수- ... -13번점수" 형식으로 저장
 
     def __str__(self):
         return self.name
